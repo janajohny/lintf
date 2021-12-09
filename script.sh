875ba851c045855635c84b4dd6eb87b03b6923da
janajohny <56085771+janajohny@users.noreply.github.com>
2021-12-09 18:05:29 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-az login --tenant-id 4f008545-7eb1-44e9-b7f4-ea0f30b4e798
+sudo az login --tenant-id 4f008545-7eb1-44e9-b7f4-ea0f30b4e798
 sudo touch /home/linuxadmin/hosts
 sudo echo -e "[ansiblevm]\n$(az vm show -d -g TF-RG -n tf-linuxvm --query privateIps -o tsv)" > /home/linuxadmin/hosts
 sudo cat /home/linuxadmin/hosts
